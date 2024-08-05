3eafbd4951f0018b9e73903d9aa09068d09d8869
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-08-05 05:32:22 +0000

    Implement profile editing functionality

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -16,3 +16,11 @@ class ProfileSerializer(serializers.ModelSerializer):
             'id', 'owner', 'created_at', 'updated_at', 'name',
             'content', 'image', 'is_owner'
         ]
+        read_only_fields = ['owner']
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.content = validated_data.get('content', instance.content)
+        instance.image = validated_data.get('image', instance.image)
+        instance.save()
+        return instance
--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, generics, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
 from happy_carpenter_api.permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 
 
 class ProfileList(APIView):
@@ -12,12 +14,14 @@ class ProfileList(APIView):
     List all profiles
     No Create view (post method), as profile creation handled by django signals
     """
+    permission_classes = [IsAuthenticatedOrReadOnly]
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(
             profiles, many=True, context={'request': request}
         )
         return Response(serializer.data)
+
 
 
 class ProfileDetail(APIView):
