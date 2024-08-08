fb5f3a73029a7c6f012a9a6de3b3f66d67154fc4
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-08-08 05:59:38 +0000

    Install react-bootstrab

--- happy-carpenter-frontend/src/App.js
+++ happy-carpenter-frontend/src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import { Container, Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hallo Carpenter</h1>
+      <Button variant="primary">Click me</Button>
     </div>
   );
 }
