2e41cdff597586e3defe1f5188908f49ea9a56d4
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-08-23 20:51:59 +0000

    Update main urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
     path('register/', RegisterView.as_view(), name='auth_register'),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', RegisterView.as_view(), name='rest_register'),
 ]
