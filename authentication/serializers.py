5a1cd23e1e0d93489e6d016b0ccbfea60a748916
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-08-23 14:02:55 +0000

    Fix profile registration data, update auth serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -8,24 +8,24 @@ from profiles.serializers import ProfileSerializer
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
-        required=False,
+        required=True,
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
-    
+
     # Profile fields
     user_type = serializers.ChoiceField(choices=Profile.USER_TYPE_CHOICES, required=True, write_only=True)
-    years_of_experience = serializers.IntegerField(required=False, write_only=True)
-    specialties = serializers.CharField(required=False, write_only=True)
-    portfolio_url = serializers.URLField(required=False, write_only=True)
+    years_of_experience = serializers.IntegerField(required=False, allow_null=True, write_only=True)
+    specialties = serializers.CharField(required=False, allow_blank=True, write_only=True)
+    portfolio_url = serializers.URLField(required=False, allow_blank=True, write_only=True)
     interests = serializers.ListField(child=serializers.CharField(max_length=100), required=False, write_only=True)
-    address = serializers.CharField(max_length=255, required=False, write_only=True)
+    address = serializers.CharField(max_length=255, required=False, allow_blank=True, write_only=True)
     profile_image = serializers.ImageField(required=False, write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name',
+        fields = ('username', 'email', 'password', 'password2', 'first_name', 'last_name',
                   'user_type', 'years_of_experience', 'specialties', 'portfolio_url',
                   'interests', 'address', 'profile_image')
         extra_kwargs = {
@@ -38,7 +38,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({"password": "Password fields didn't match."})
         
         if attrs['user_type'] == 'professional':
-            if not attrs.get('years_of_experience'):
+            if attrs.get('years_of_experience') is None:
                 raise serializers.ValidationError({"years_of_experience": "This field is required for professional users."})
             if not attrs.get('specialties'):
                 raise serializers.ValidationError({"specialties": "This field is required for professional users."})
@@ -47,7 +47,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         # Remove profile-specific data
-        user_type = validated_data.pop('user_type', 'amateur')
+        user_type = validated_data.pop('user_type')
         years_of_experience = validated_data.pop('years_of_experience', None)
         specialties = validated_data.pop('specialties', '')
         portfolio_url = validated_data.pop('portfolio_url', '')
@@ -96,3 +96,4 @@ class RegisterSerializer(serializers.ModelSerializer):
             'refresh': instance['refresh'],
             'access': instance['access'],
         }
+        
