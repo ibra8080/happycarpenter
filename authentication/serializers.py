4ab3c1117fa1a4f8f999bf765f985ff84532af2e
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-09-25 19:49:57 +0000

    Adjust the serializer to make the profile optional

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -21,30 +21,49 @@ class RegisterSerializer(serializers.ModelSerializer):
     last_name = serializers.CharField(required=True)
 
     # Profile fields
-    profile = ProfileSerializer(required=True)
+    user_type = serializers.ChoiceField(choices=Profile.USER_TYPE_CHOICES, required=False)
+    years_of_experience = serializers.IntegerField(required=False, allow_null=True)
+    specialties = serializers.CharField(required=False, allow_blank=True)
+    portfolio_url = serializers.URLField(required=False, allow_blank=True)
+    interests = serializers.ListField(child=serializers.CharField(max_length=100), required=False)
+    address = serializers.CharField(max_length=255, required=False, allow_blank=True)
+    content = serializers.CharField(required=False, allow_blank=True)
+    image = serializers.ImageField(required=False, allow_null=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'profile')
+        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name',
+                  'user_type', 'years_of_experience', 'specialties', 'portfolio_url',
+                  'interests', 'address', 'content', 'image')
 
     def validate(self, attrs):
         if attrs['password1'] != attrs['password2']:
             raise serializers.ValidationError({"password": "Password fields didn't match."})
 
-        profile_data = attrs.get('profile', {})
-        if profile_data.get('user_type') == 'professional':
-            if profile_data.get('years_of_experience') is None:
+        if attrs.get('user_type') == 'professional':
+            if attrs.get('years_of_experience') is None:
                 raise serializers.ValidationError({"years_of_experience": "This field is required for professional users."})
-            if not profile_data.get('specialties'):
+            if not attrs.get('specialties'):
                 raise serializers.ValidationError({"specialties": "This field is required for professional users."})
 
         return attrs
 
     def create(self, validated_data):
         try:
-            profile_data = validated_data.pop('profile')
             password = validated_data.pop('password1')
             validated_data.pop('password2', None)
+
+            # Extract profile data
+            profile_data = {
+                'user_type': validated_data.pop('user_type', 'amateur'),
+                'years_of_experience': validated_data.pop('years_of_experience', None),
+                'specialties': validated_data.pop('specialties', ''),
+                'portfolio_url': validated_data.pop('portfolio_url', ''),
+                'interests': validated_data.pop('interests', []),
+                'address': validated_data.pop('address', ''),
+                'content': validated_data.pop('content', ''),
+                'image': validated_data.pop('image', None),
+            }
 
             user = User.objects.create_user(
                 username=validated_data['username'],
@@ -54,7 +73,8 @@ class RegisterSerializer(serializers.ModelSerializer):
                 last_name=validated_data.get('last_name', '')
             )
 
-            Profile.objects.create(owner=user, **profile_data)
+            # Create or update profile
+            Profile.objects.update_or_create(owner=user, defaults=profile_data)
 
             return user
         except Exception as e:
