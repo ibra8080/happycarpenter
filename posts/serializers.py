09b59fee03fb80f5af4f4da7c9f0c35c3df04d18
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-10-02 08:50:55 +0000

    Update comment serializer with user's data new format

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,6 +4,7 @@ from django.contrib.humanize.templatetags.humanize import naturaltime
 from cloudinary.utils import cloudinary_url
 import logging
 
+
 logger = logging.getLogger(__name__)
 
 
@@ -14,19 +15,29 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
 
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
     def get_created_at(self, obj):
-        return naturaltime(obj.created_at)
+        return obj.created_at.isoformat()
 
     def get_updated_at(self, obj):
-        return naturaltime(obj.updated_at)
+        return obj.updated_at.isoformat()
 
     class Meta:
         model = Comment
-        fields = ['id', 'owner', 'post', 'created_at', 'updated_at', 'content']
-
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'post', 'created_at', 'updated_at', 'content'
+        ]
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
