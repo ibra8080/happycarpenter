137c3e41096b949098ec86f8b31aaf17bfa6f075
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-10-10 13:09:22 +0000

    Updatee professinals admin with its models

--- professionals/admin.py
+++ professionals/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import JobOffer, Advertisement, Review
 
-# Register your models here.
+admin.site.register(JobOffer)
+admin.site.register(Advertisement)
+admin.site.register(Review)
