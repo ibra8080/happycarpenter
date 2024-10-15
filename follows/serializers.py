b8ea824c48695cf250175839a9d1ac7e1614b057
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-10-15 20:36:42 +0000

    Update follow serializer with followed field

--- follows/serializers.py
+++ follows/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Follow
-
+from django.contrib.auth.models import User
 
 class FollowSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -11,3 +11,14 @@ class FollowSerializer(serializers.ModelSerializer):
         fields = ['id', 'owner', 'followed', 'followed_name', 'created_at']
         read_only_fields = ['owner']
 
+    def validate_followed(self, value):
+        if value == self.context['request'].user:
+            raise serializers.ValidationError("You cannot follow yourself.")
+        return value
+
+    def create(self, validated_data):
+        followed = validated_data.get('followed')
+        owner = self.context['request'].user
+        if Follow.objects.filter(owner=owner, followed=followed).exists():
+            raise serializers.ValidationError("You are already following this user.")
+        return Follow.objects.create(**validated_data)
