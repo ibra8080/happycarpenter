19fc10f3fa23662127a7c2f9b58beb70c905b5b8
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-10-15 20:29:47 +0000

    Update follow views with corrected the get_object method

--- follows/views.py
+++ follows/views.py
@@ -2,33 +2,19 @@ from rest_framework import generics, permissions
 from happy_carpenter_api.permissions import IsOwnerOrReadOnly
 from .models import Follow
 from .serializers import FollowSerializer
-from rest_framework.response import Response
-from django.contrib.auth.models import User
 
 class FollowList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Follow.objects.all()
     serializer_class = FollowSerializer
+    queryset = Follow.objects.all()
 
     def perform_create(self, serializer):
-        followed_id = self.request.data.get('followed')
-        try:
-            followed_user = User.objects.get(id=followed_id)
-            serializer.save(owner=self.request.user, followed=followed_user)
-        except User.DoesNotExist:
-            raise serializers.ValidationError("User to follow does not exist.")
-
-    def create(self, request, *args, **kwargs):
-        try:
-            return super().create(request, *args, **kwargs)
-        except serializers.ValidationError as e:
-            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
+        serializer.save(owner=self.request.user)
 
 class FollowDetail(generics.RetrieveDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Follow.objects.all()
     serializer_class = FollowSerializer
+    queryset = Follow.objects.all()
 
     def get_object(self):
         queryset = self.filter_queryset(self.get_queryset())
@@ -36,6 +22,6 @@ class FollowDetail(generics.RetrieveDestroyAPIView):
             'owner': self.request.user,
             'followed__id': self.kwargs['pk']
         }
-        obj = get_object_or_404(queryset, **filter_kwargs)
+        obj = generics.get_object_or_404(queryset, **filter_kwargs)
         self.check_object_permissions(self.request, obj)
         return obj
