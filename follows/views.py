a17d7e1803384d2b6f2687609d110cf230399f5d
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-10-16 10:06:35 +0000

    Adjustment to filter follows in the follow views

--- follows/views.py
+++ follows/views.py
@@ -1,19 +1,29 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from happy_carpenter_api.permissions import IsOwnerOrReadOnly
 from .models import Follow
 from .serializers import FollowSerializer
 
 class FollowList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = FollowSerializer
-    queryset = Follow.objects.all()
+
+    def get_queryset(self):
+        queryset = Follow.objects.all()
+        followed = self.request.query_params.get('followed')
+        owner = self.request.query_params.get('owner')
+        
+        if followed:
+            queryset = queryset.filter(followed__username=followed)
+        if owner:
+            queryset = queryset.filter(owner__username=owner)
+        
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 class FollowDetail(generics.RetrieveDestroyAPIView):
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = FollowSerializer
     queryset = Follow.objects.all()
 
@@ -21,16 +31,8 @@ class FollowDetail(generics.RetrieveDestroyAPIView):
         queryset = self.filter_queryset(self.get_queryset())
         filter_kwargs = {
             'owner': self.request.user,
-            'followed__id': self.kwargs['pk']
+            'followed__username': self.kwargs['pk']
         }
-        obj = get_object_or_404(queryset, **filter_kwargs)
+        obj = generics.get_object_or_404(queryset, **filter_kwargs)
         self.check_object_permissions(self.request, obj)
         return obj
-
-    def destroy(self, request, *args, **kwargs):
-        try:
-            instance = self.get_object()
-            self.perform_destroy(instance)
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        except Http404:
-            return Response({"detail": "Follow relationship not found."}, status=status.HTTP_404_NOT_FOUND)
