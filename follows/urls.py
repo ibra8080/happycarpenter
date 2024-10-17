ad1bf4ec3a9f57b2addbfc1ea5e0bec08b1580fc
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-10-17 06:31:01 +0000

    Clean follows urls

--- follows/urls.py
+++ follows/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from follows import views
+from . import views
 
 urlpatterns = [
-    path('follows/', views.FollowList.as_view()),
-    path('follows/<int:pk>/', views.FollowDetail.as_view()),
+    path('follows/', views.FollowList.as_view(), name='follow-list'),
     path('follows/<str:pk>/', views.FollowDetail.as_view(), name='follow-detail'),
 ]
