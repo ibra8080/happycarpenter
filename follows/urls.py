f22060263778dbe3337cc5d34e623059e80f0934
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-10-17 04:29:29 +0000

    update follows urls with follow detail link

--- follows/urls.py
+++ follows/urls.py
@@ -4,4 +4,5 @@ from follows import views
 urlpatterns = [
     path('follows/', views.FollowList.as_view()),
     path('follows/<int:pk>/', views.FollowDetail.as_view()),
+    path('follows/<str:pk>/', views.FollowDetail.as_view(), name='follow-detail'),
 ]
